c4bd665ae5a6f5d1ae0162d05ccb7bad007547d1
Eduardo Almeida <web2solucoes@gmail.com>
2021-04-12 11:52:35 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
     
     let secret = rand::thread_rng().gen_range(1..101);
-    // println!("The secret number is: {}", secret);
 
     loop {
         println!("Please input your guess.");
